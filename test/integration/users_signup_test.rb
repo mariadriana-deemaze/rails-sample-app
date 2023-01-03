e11e9232b9b457d5f528ae38f83f7253a394a0a9
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-01-03 12:17:56 +0000

    Assert render of errors banner on unsuccessful sign up test

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -10,5 +10,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                           password_confirmation:"bar" } }
     end
     assert_template 'users/new'
+    assert_select 'div.banner.banner-alert'
   end
 end
