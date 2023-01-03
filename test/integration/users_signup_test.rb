ecf1a13056686cc77a4c520a189d9063312c380b
Maria Adriana <maria.adriana.cardoso@deemaze.com>
2023-01-03 13:59:33 +0000

    Assert successful sign up

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,4 +12,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_template 'users/new'
     assert_select 'div.banner.banner-danger'
   end
+
+  test "Valid signup" do
+    get signup_path
+    assert_difference "User.count", 1 do
+      post users_path, params: { user: {  name: Faker::Name.name,
+        email: Faker::Internet.safe_email,
+        password: "foobar",
+        password_confirmation:"foobar" } }
+    end
+    follow_redirect!
+    assert_template "users/show"
+    assert flash[:success]
+  end
 end
